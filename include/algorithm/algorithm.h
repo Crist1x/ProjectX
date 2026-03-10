b986f61b279153cb3094d9e92e6448f3dcd3e0f2
ilyal <ilya.lis2601@mail.ru>
2026-03-10 21:01:55 +0300

    test comm

---
+++ include/algorithm/algorithm.h
@@ -0,0 +1,8 @@
+//
+// Created by ilyal on 3/10/2026.
+//
+
+#ifndef PROJECT_ALGORITHM_H
+#define PROJECT_ALGORITHM_H
+
+#endif //PROJECT_ALGORITHM_H
---
+++ src/algorithm/alg.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ilyal on 3/10/2026.
+//
+
+
+#include <iostream>
+
+void test(int x) {
+    return x;
+}
+
