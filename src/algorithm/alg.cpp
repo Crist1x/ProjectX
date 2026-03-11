a6a7a5e57432c7448d56bc2f2a8ebe2ab31b53e3
ilyal <ilya.lis2601@mail.ru>
2026-03-11 19:13:26 +0300

    alg cpp 1

--- src/algorithm/alg.cpp
+++ src/algorithm/alg.cpp
@@ -1,101 +1,21 @@
-#include "../menu/MenuItem.h"
-#include <vector>
-#include <deque>
-#include <string>
-#include <chrono>
+#include "algorithm/OrderDistributionAlgorithm.h"
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <limits>
 
-struct Order {
-    std::string id;
-    std::vector<Item> items;
-    double totalPreparationTime;  // общее время приготовления заказа
-    double estimatedReadyTime;    // предполагаемое время готовности
-    int assignedBaristaId;        // ID бариста, который готовит
-    std::chrono::system_clock::time_point orderTime;  // время заказа
+OrderDistributionAlgorithm::OrderDistributionAlgorithm() 
+    : currentTime(0), workingDayStartTime(0) {}
 
-    Order() : totalPreparationTime(0), estimatedReadyTime(0), assignedBaristaId(-1) {}
-};
+void OrderDistributionAlgorithm::initializeBaristas(int totalBaristas) {
+    baristas.clear();
+    baristas.resize(totalBaristas);
+    
+    for (int i = 0; i < totalBaristas; ++i) {
+        baristas[i].id = i;
+        baristas[i].isWorking = false;
+        baristas[i].busyUntilTime = workingDayStartTime;
+        baristas[i].totalOrdersCompleted = 0;
+    }
+}
 
-struct Barista {
-    int id;
-    bool isWorking;
-    std::deque<Order> orderQueue;  // очередь заказов
-    double busyUntilTime;           // время, до которого бариста занят
-    int totalOrdersCompleted;       // статистика
-
-    Barista() : id(0), isWorking(false), busyUntilTime(0), totalOrdersCompleted(0) {}
-};
-
-class OrderDistributionAlgorithm {
-private:
-    std::vector<Barista> baristas;
-    std::deque<Order> commonQueue;  // общая очередь заказов
-    double currentTime;              // текущее время симуляции
-    double workingDayStartTime;      // начало рабочего дня (в минутах от 00:00)
-
-    // Расчет времени приготовления одного элемента меню
-    double calculateItemPreparationTime(const Item& item);
-
-    // Поиск бариста с наименьшей загрузкой
-    int findLeastLoadedBarista();
-
-    // Поиск бариста с самой короткой очередью
-    int findBaristaWithShortestQueue();
-
-public:
-    OrderDistributionAlgorithm();
-
-    // ========== ИНИЦИАЛИЗАЦИЯ ==========
-
-    // Установка количества бариста
-    void initializeBaristas(int totalBaristas);
-
-    // Установка статуса работы для бариста
-    void setBaristaStatus(int baristaId, bool isWorking);
-
-    // Установка времени начала рабочего дня
-    void setWorkingDayStart(double startTimeInMinutes);
-
-    // ========== УПРАВЛЕНИЕ ЗАКАЗАМИ ==========
-
-    // Добавление заказа в общую очередь
-    void addOrderToCommonQueue(const Order& order);
-
-    // Распределение заказов из общей очереди по бариста
-    void distributeOrders();
-
-    // Распределение одного заказа конкретному бариста
-    void assignOrderToBarista(Order& order, int baristaId);
-
-    // ========== РАСЧЕТ ВРЕМЕНИ ==========
-
-    // Расчет времени приготовления заказа
-    double calculateOrderPreparationTime(const Order& order);
-
-    // Получение общего времени выполнения всех заказов
-    double getTotalProcessingTime() const;
-
-    // Получение среднего времени ожидания
-    double getAverageWaitTime() const;
-
-    // ========== СТАТИСТИКА ==========
-
-    // Получение статистики по бариста
-    std::vector<Barista> getBaristas() const;
-
-    // Получение очереди конкретного бариста
-    std::deque<Order> getBaristaQueue(int baristaId) const;
-
-    // Получение размера общей очереди
-    size_t getCommonQueueSize() const;
-
-    // Количество работающих бариста
-    int getWorkingBaristasCount() const;
-
-    // ========== УТИЛИТЫ ==========
-
-    // Сброс состояния алгоритма
-    void reset();
-
-    // Вывод статистики в консоль
-    void printStatistics() const;
-};
